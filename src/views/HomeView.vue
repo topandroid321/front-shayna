78d4b20bcc81cc8f39f2c16e1211e3a76342c24f
topandroid <topandroid@pop-os.localdomain>
2022-09-19 20:24:32 +0700

    menambah component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HeroShayna/>
+    <WomanShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomanShayna from '@/components/WomanShayna.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna
+  }
+}
+</script>
